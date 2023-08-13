e3ab335fedabe29d6ab97c201fb906685b1cd647
Erik Olofsson <erik@olofsson.info>
2023-08-13 11:34:02 +0200

    Disable heap checker to make it work on macOS

--- src/gperftools/heap-checker.h
+++ src/gperftools/heap-checker.h
@@ -195,7 +195,7 @@ class PERFTOOLS_DLL_DECL HeapLeakChecker {
   // whitelist objects referenced through it.
   template <typename T>
   static T* IgnoreObject(T* ptr) {
-    DoIgnoreObject(static_cast<const void*>(const_cast<const T*>(ptr)));
+    //DoIgnoreObject(static_cast<const void*>(const_cast<const T*>(ptr)));
     return ptr;
   }
 
